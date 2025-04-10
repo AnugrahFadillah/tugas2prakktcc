040400b59a4d4e6b755c990b89f78e9aeb051382
AnugrahFadillah <161240712+AnugrahFadillah@users.noreply.github.com>
2025-04-10 18:07:46 +0700

    Update index.js

--- notes-backend/index.js
+++ notes-backend/index.js
@@ -1,13 +1,11 @@
 import express from "express";
 import cors from "cors";
-import UserRoute from "./route/NotesRoute.js";
+import notesRouter from "./route/NotesRoute.js";
 
 const app = express();
-
 app.use(cors());
 app.use(express.json());
-app.get("/", (req, res) => res.render("index"));
-app.use(UserRoute);
+app.use(notesRouter);
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log(`Server connected on port ${PORT}`));
