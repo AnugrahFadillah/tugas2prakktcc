d40f85e7cf714eafc4b773f28c27a36f0b64ddad
AnugrahFadillah <161240712+AnugrahFadillah@users.noreply.github.com>
2025-04-10 16:57:15 +0700

    Update index.js

--- notes-backend/index.js
+++ notes-backend/index.js
@@ -19,7 +19,7 @@ app.use(notesRoutes);
         await db.sync(); // Memastikan tabel dibuat jika belum ada
         console.log("Database Connected!");
 
-        const PORT = process.env.PORT || 5000;
+        const PORT = process.env.PORT || 8080;
         app.listen(PORT, () => {
             console.log(`Server running at http://localhost:${PORT}`);
         });
