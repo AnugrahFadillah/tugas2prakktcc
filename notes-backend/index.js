c003f134dad242260e5b07b523b1843bbacc6d6c
AnugrahFadillah <161240712+AnugrahFadillah@users.noreply.github.com>
2025-04-10 18:04:34 +0700

    Update index.js

--- notes-backend/index.js
+++ notes-backend/index.js
@@ -1,6 +1,6 @@
 import express from "express";
 import cors from "cors";
-import UserRoute from "./routes/UserRoute.js";
+import UserRoute from "./route/UserRoute.js";
 
 const app = express();
 app.set("view engine", "ejs");
