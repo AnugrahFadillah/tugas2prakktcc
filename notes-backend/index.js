eb52d3704b093fd103ac58e6bb32a6d4ca7ed255
AnugrahFadillah <161240712+AnugrahFadillah@users.noreply.github.com>
2025-04-14 00:23:21 +0700

    Update index.js

--- notes-backend/index.js
+++ notes-backend/index.js
@@ -1,29 +1,10 @@
 import express from "express";
-import db from "./config/database.js";
-import notesRoutes from "./route/NotesRoute.js";
 import cors from "cors";
+import notesRouter from "./route/notesRoute.js";
 
-const app = express(); // Pastikan app dideklarasikan sebelum digunakan
-const PORT = 8080;
-
-// Middleware
+const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(notesRouter);
 
-// Routes
-app.use(notesRoutes);
-
-// Test database connection and start server
-(async () => {
-    try {
-        await db.authenticate();
-        await db.sync(); // Memastikan tabel dibuat jika belum ada
-        console.log("Database Connected!");
-
-        app.listen(PORT, () => {
-            console.log(`Server running at http://localhost:${PORT}`);
-        });
-    } catch (error) {
-        console.error("Connection error:", error);
-    }
-})();
+app.listen(5001, () => console.log("Server Berjalan"));
