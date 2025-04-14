fc8cb07e51cdf62749078e49796927eb2f6bb4df
AnugrahFadillah <161240712+AnugrahFadillah@users.noreply.github.com>
2025-04-14 20:47:48 +0700

    Update index.js

--- notes-backend/index.js
+++ notes-backend/index.js
@@ -7,5 +7,5 @@ app.use(cors());
 app.use(express.json());
 app.use(notesRouter);
 
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server Berjalan di port ${PORT}`));
